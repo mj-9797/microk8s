95e402dc275f329285ce00ff5480e84f35410b8b
mj-9797 <70877266+mj-9797@users.noreply.github.com>
2020-09-21 10:16:15 +0530

    Update validators.py

--- tests/validators.py
+++ tests/validators.py
@@ -253,8 +253,8 @@ def validate_registry():
     pvc_yaml = yaml.safe_load(pvc_stdout)
     storage = pvc_yaml['spec']['resources']['requests']['storage']
     assert re.match("(^[2-9][0-9]{1,}|^[1-9][0-9]{2,})(Gi$)", storage)
-    docker("pull busybox")
-    docker("tag busybox localhost:32000/my-busybox")
+    docker("pull busybox:1.28.2-musl")
+    docker("tag busybox:1.28.2-musl localhost:32000/my-busybox")
     docker("push localhost:32000/my-busybox")
 
     here = os.path.dirname(os.path.abspath(__file__))
